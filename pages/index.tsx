82633f311e47866c3d6a2067ab4076c67a0e3ca0
JinleeJeong <pjjr0118@gmail.com>
2023-02-12 16:04:16 +0900

    fix: default export error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { HomeScreen } from "@/Home";
-import { ReactElement } from "react";
-import { NextLayoutPage } from "./_app";
 
-export const HomePage: NextLayoutPage = () => <HomeScreen />;
+export default function HomePage() {
+  <HomeScreen />;
+}
