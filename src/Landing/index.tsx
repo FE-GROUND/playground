6130c553de3959ddcb670f2013095b212335a41e
JinleeJeong <pjjr0118@gmail.com>
2023-02-16 10:52:17 +0900

    feature: create login button to show entire flow

--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -1,3 +1,30 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Fragment } from "react";
+
 export const LandingScreen = () => {
-  return null;
+  const { data, status } = useSession();
+  return (
+    <Fragment>
+      <main>
+        <p>status: {status}</p>
+
+        {/* TODO 마크업 수정 예정 */}
+        <p>{data?.user?.name}</p>
+        {!data?.user ? (
+          <>
+            <button type="button" onClick={() => signIn("google")}>
+              Google Login
+            </button>
+            <button type="button" onClick={() => signIn("kakao")}>
+              Kakao Login
+            </button>
+          </>
+        ) : (
+          <button type="button" onClick={() => signOut()}>
+            Logout
+          </button>
+        )}
+      </main>
+    </Fragment>
+  );
 };
