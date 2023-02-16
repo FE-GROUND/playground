330fda5eedf1c7d06f1b15545e2b6255b406775b
JinleeJeong <pjjr0118@gmail.com>
2023-02-16 10:23:43 +0900

    feature: connect client info on Google Provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
     // ...add more providers here
   ],
